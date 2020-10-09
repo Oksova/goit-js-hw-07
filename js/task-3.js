d793ecdbb04f760a333ba67bd44c197b12d20e0a
Mariia Oksova <oksova027@gmail.com>
2020-10-09 15:52:12 +0300

    Добавлены изменения в таск 3

--- js/task-3.js
+++ js/task-3.js
@@ -14,19 +14,21 @@ const images = [
       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-]
+];
 
-const galleryEl = document.querySelector('#gallery')
-galleryEl.classList.add('img')
-galleryEl.style.display = 'flex'
-galleryEl.style.listStyle = 'none'
-console.log(galleryEl)
+const galleryEl = document.querySelector('#gallery');
+galleryEl.classList.add('img');
+galleryEl.style.display = 'flex';
+galleryEl.style.listStyle = 'none';
+console.log(galleryEl);
 
-const imagesEl = images.map((image) => {
+const imagesEl = images.map(image => {
   galleryEl.insertAdjacentHTML(
     'beforeEnd',
     `<li><img src="${image.url}" alt="${image.alt}" width = 200 height = 150></li>`,
-  )
-})
-const itemEl = document.querySelectorAll('li')
-itemEl.classList.add('item')
+  );
+  console.log(imagesEl);
+});
+const itemEl = document.querySelectorAll('li');
+itemEl.classList.add('item');
+console.log(itemEl);
